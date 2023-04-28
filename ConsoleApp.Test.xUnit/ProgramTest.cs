f17868defeec54d4b7e35fc51956c4729d0a3b1b
Paweł Łęczycki <pawel@msd.info.pl>
2023-04-28 14:27:43 +0200

    Main

---
+++ ConsoleApp.Test.xUnit/ProgramTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Test.xUnit
+{
+    public class ProgramTest
+    {
+        [Fact]
+        public void Main_ConsoleOutput_HelloWorld()
+        {
+            //Arrange
+            var main = typeof(Program).Assembly.EntryPoint;
+            var stringWriter = new StringWriter();
+            var originalConsoleOutput = Console.Out;
+            Console.SetOut(stringWriter);
+
+            //Act
+            //Program.Main();
+            main.Invoke(null, new object[] { Array.Empty<string>()});
+
+            //
+            Console.SetOut(originalConsoleOutput);
+
+            Assert.Equal("Hello, World!\r\n", stringWriter.ToString());
+            stringWriter.Dispose();
+
+        }
+
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine();
+﻿
+//internal partial class Program {
+
+//    static void Main(string[] args)
+//    {
+        Console.WriteLine("Hello, World!");
+//    }
+//}
+
+public partial class Program
+{
+
+}

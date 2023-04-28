b1f01eb7cad0be0d823d21edde393b09d5107163
Paweł Łęczycki <pawel@msd.info.pl>
2023-04-28 12:18:32 +0200

    Czas wykonywania

--- ConsoleApp.Test.NUnit/FizzBuzzTest.cs
+++ ConsoleApp.Test.NUnit/FizzBuzzTest.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FluentAssertions;
+using FluentAssertions.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -86,6 +88,20 @@ namespace ConsoleApp.Test.NUnit
             Assert.IsTrue(zip.Where(x => !x.First.Contains("Fizz"))
                              .Where(x => !x.First.Contains("Buzz"))
                              .All(x => x.First == x.Second));
+        }
+
+        [Test]
+        public void Compute_10000_ExecutionTimeBelow1s()
+        {
+            //Arrange
+            const int COUNT = 5000000;
+
+            //Act
+            Action action = () => FizzBuzz.Compute(COUNT);
+
+            //Assert
+            action.ExecutionTime().Should().BeLessThan(1.Seconds());
+
         }
     }
 }
